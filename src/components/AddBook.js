b10b77c007d3cb3e39e0cb8bf19b2c171d558417
Adilet312 <adiletm@hotmail.com>
2020-10-27 14:28:06 -0700

    Add toggle for form to display or hide when user is clicked.

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -5,12 +5,23 @@ const AddBook = ({sendBook}) =>{
   const [ author, setAuthor] = useState('');
   const [ pages, setPages] = useState(0);
   return(
-  <div>
-      <div className = "add-book"><h3>Add Book</h3></div>
-      <form className = "login-form" onSubmit = {(e)=> sendBook(e,{ title: title, author: author, pages: pages})}>
-        <input type = "text" placeholder = "Title" name = "title" value = {title} className = "title" onChange = {(e)=>setTitle(e.target.value)}/>
-        <input type = "text" placeholder = "Author" name = "author" value = {author} className = "author" onChange = {(e)=>setAuthor(e.target.value)}/>
-        <input type = "number" placeholder = "Pages" name = "pages" value = {pages} className = "pages" onChange = {(e) => setPages(e.target.value)}/>
+  <div className = "add-form">
+      <div className = "add-book">
+      <h3  onClick = {(event) => {
+        let display = event.target.parentNode.parentNode.classList;
+        display.toggle('add-form');
+      }}>+ Add Book</h3>
+      </div>
+      <form className = "login-form" onSubmit = {(e)=> {
+        sendBook(e,{ title: title, author: author, pages: pages});
+        setTitle('');
+        setAuthor('');
+        setPages(0);
+        }
+      }>
+        <input type = "text" placeholder = "Title" value = {title} name = "title" className = "title" onChange = {(e)=>setTitle(e.target.value)}/>
+        <input type = "text" placeholder = "Author" value = {author} name = "author"  className = "author" onChange = {(e)=>setAuthor(e.target.value)}/>
+        <input type = "number" placeholder = "Pages" value = {pages} name = "pages" className = "pages" onChange = {(e) => setPages(e.target.value)}/>
         <input type = "submit" className = "btn" name = "login" value = "Add book"/>
       </form>
 </div>
