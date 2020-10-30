7940941b9c93945cd5f4cc0ede9adcd94ebfd0be
Adilet312 <adiletm@hotmail.com>
2020-10-29 23:27:14 -0700

    Add Search component remember the most recent search interaction.

--- src/components/SearchedBooks.js
+++ src/components/SearchedBooks.js
@@ -1,12 +1,19 @@
-import React,{ useState } from 'react';
+import React,{ useState,useEffect } from 'react';
 import Library from './Library.js';
 import './css/SearchedBooks.css';
 const SearchedBooks = ({books,deleteBook,updateBookProperties}) =>{
-  const [ getInput, setInput ] = useState('');
+  let local_storage = window.localStorage;
+  const [ getInput, setInput ] = useState(local_storage.getItem('search') || ' ');
   let searchBooks = books.filter( book => book.title.toLowerCase().includes(getInput.toLowerCase()));
+  useEffect( () => {
+    local_storage.setItem('search',getInput);
+  },[getInput])
   return(
    <div className = 'searchBox'>
-    <input type = 'search' className ="inputData" placeholder = "Search" onChange = {(event) => setInput(event.target.value)}/>
+    <input type = 'search' className ="inputData" placeholder = "Search" onChange = {(event) => {
+      setInput(event.target.value);
+
+    }}/>
     <Library books = {searchBooks} deleteBook = {deleteBook} updateBookProperties = {updateBookProperties}/>
    </div>
   )
